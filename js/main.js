22b39af26619c5707fa5f7d6a4b7b604ec9ddd0c
alfaisaldev <faisalbiswas229@gmail.com>
2023-04-25 00:18:00 +0600

    new

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",
+{
+    strings: ["Full Stack Web Developer.","Graphics Designer.","Animator.","Android Developer.","Android Developer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
